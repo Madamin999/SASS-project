f46799cf5fdcdc3e77351bc99a7af84c85ae1b75
Madamin999 <madamink555@gmail.com>
2021-12-12 15:02:56 +0600

    initial

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
-
 $ ('.slider-2').slick({
     slidesToShow: 5,
     dots:false,
-    autoplay:true,
+    autoplay:false,
+    arrows:false,
     responsive: [
         {
-            breakpoint:769,
+            breakpoint:1024,
             settings:{
+                slidesToShow:4,
                 arrows: false,
                 centerPadding: "40px",
-                slidesToShow:3
+
             }
         }
     ]
